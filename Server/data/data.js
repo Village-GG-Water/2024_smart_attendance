80d3e0ce344c92f6fd8d538fddd5755eac52d603
hw-ani <pcbmlh73@gmail.com>
2024-04-05 15:40:53 +0900

    Refactor: mysql dotenv 제거, inmemory 저장
    - 회의결과 굳이 mysql 사용할 필요가 없다고 느껴서 memory에 올리는 방식으로 변경
    - mysql, dotenv 모듈 제거

---
+++ Server/data/data.js
@@ -0,0 +1,22 @@
+let data = [
+  {
+    subject_id: 1,
+    subject_name: 'Algorithm',
+    students: [
+      { name: '박지환', id: '2019115876' },
+      { name: '홍길동', id: '2020754876' },
+    ],
+    attendance_code: {},
+  },
+  {
+    subject_id: 2,
+    subject_name: 'Operating System',
+    students: [
+      { name: '박지환', id: '2019115876' },
+      { name: '홍길동', id: '2020754876' },
+    ],
+    attendance_code: {},
+  },
+];
+
+module.exports = data;
