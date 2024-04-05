a93642ff33852626e8bae2cf0b8cb066e6086213
hw-ani <pcbmlh73@gmail.com>
2024-04-05 16:09:42 +0900

    Chore: dotenve 코드 제거, 데이터 id 변경
    - dotenv 관련 코드 제거
    - 데이터 id를 0부터 시작하도록 변경

--- Server/data/data.js
+++ Server/data/data.js
@@ -1,6 +1,9 @@
+// 주의
+// subject_id는 겹치지 않도록 지정할 것
+
 let data = [
   {
-    subject_id: 1,
+    subject_id: 0,
     subject_name: 'Algorithm',
     students: [
       { name: '박지환', id: '2019115876' },
@@ -9,7 +12,7 @@ let data = [
     attendance_code: {},
   },
   {
-    subject_id: 2,
+    subject_id: 1,
     subject_name: 'Operating System',
     students: [
       { name: '박지환', id: '2019115876' },
