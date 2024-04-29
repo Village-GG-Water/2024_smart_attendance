15c8026b28cbd5f028b7f6fca95667e391015ead
hw-ani <pcbmlh73@gmail.com>
2024-04-29 18:57:28 +0900

    Feat: 메인 로직(음파 생성, 검증) 통합
    - 메인 로직 코드를 기존 서버 코드에 통합

--- Server/public/javascripts/prof-subject.js
+++ Server/public/javascripts/prof-subject.js
@@ -3,6 +3,7 @@ let buttonFlag = false;
 function changeButtonOn(btn) {
   btn.innerHTML = '음파 생성 중';
   btn.style.backgroundColor = '#4CAF50';
+  startAttendanceCheck(btn.id);
 }
 
 function changeButtonOff(btn) {
@@ -44,3 +45,135 @@ document.addEventListener('DOMContentLoaded', function () {
 
   setInterval(updateAttendees, 1000);
 });
+
+// ---------------------------- 음파 생성 로직 ---------------------------- //
+/**
+ * 출결 체크를 시작하는 function
+ *
+ * @param {number} subjectid 과목코드
+ */
+function startAttendanceCheck(subjectid) {
+  let attendanceDuration = 300; //second
+  const startTime = Math.floor(Date.now() / 1000);
+
+  while (Math.floor(Date.now() / 1000) - startTime < attendanceDuration) {
+    let currentAttendanceCode = generateAuthCode();
+
+    const requestForm = {
+      subjectid: subjectid,
+      attendanceCode: currentAttendanceCode,
+      startTime: Math.floor(Date.now() / 1000),
+    };
+    new Request('/prof' + subjectid.toString(), {
+      method: 'POST',
+      body: JSON.stringify(requestForm),
+    });
+
+    playSignal(currentAttendanceCode);
+  }
+}
+
+/**
+ * Oscillator를 이용하여 attendanceCode를 소리로 출력하는 function
+ *
+ * @param {String} attendanceCode 음파신호로 출력해야 하는 출결 인증 코드
+ */
+function playSignal(attendanceCode) {
+  const ac = new (window.AudioContext || window.webkitAudioContext)();
+  ac.resume();
+
+  const frequencySet = codeToFrequency(attendanceCode);
+
+  const maxFrequency = 20000;
+  const unitFrequency = 50;
+  const len = Math.floor(maxFrequency / unitFrequency) + 1;
+
+  const real = new Float32Array(len);
+  const imag = new Float32Array(len);
+
+  for (let index = 0; index < frequencySet.length; index++) {
+    const element = frequencySet[index];
+
+    real[Math.floor(element / unitFrequency)] = 1;
+  }
+
+  const wave = ac.createPeriodicWave(real, imag, {
+    disableNormalization: false,
+  });
+
+  const osc = ac.createOscillator();
+  const gain = ac.createGain();
+
+  osc.frequency.value = unitFrequency;
+
+  osc.setPeriodicWave(wave);
+
+  gain.gain.value = 0.5;
+
+  osc.connect(gain).connect(ac.destination);
+
+  osc.start();
+  sleep(5000);
+  osc.stop();
+}
+
+/**
+ * 원하는 자리수의 이진 코드 생성하는 function
+ *
+ * @param {number} NumberofBits 생성할 코드 비트 자리 수 (default value = 11)
+ * @returns {string} 이진수 문자열
+ */
+function generateAuthCode(NumberofBits = 11) {
+  return Math.floor(Math.random() * 2 ** NumberofBits - 1)
+    .toString(2)
+    .padStart(NumberofBits, 0);
+}
+
+/**
+ * 이진 코드를 출력할 주파수 set으로 변환하는 function
+ *
+ * @param {string} code 출력할 이진 code
+ * @returns {Array} 출력할 주파수를 담은 Array
+ */
+function codeToFrequency(code) {
+  const minFrequency = 18000,
+    maxFrequency = 20000;
+
+  if (code.length == 0) {
+    return;
+  }
+
+  let totalFrequencySet = Array(code.length); // 해당 길이의 code가 출력할 수 있는 가능한 모든 주파수를 담은 Array
+
+  if (code.length == 1) {
+    totalFrequencySet[0] = minFrequency;
+  } else {
+    let interval = Math.floor(
+      (maxFrequency - minFrequency) / (code.length - 1)
+    ); //bit 길이에 맞도록 간격 설정
+
+    let cur = minFrequency;
+    for (let index = 0; index < totalFrequencySet.length; index++) {
+      //total_frequency_set 생성
+      totalFrequencySet[index] = cur;
+      cur += interval;
+    }
+  }
+
+  //실제 출력할 frequency_set 생성
+  let frequencySet = Array();
+  for (let index = 0; index < code.length; index++) {
+    const element = code[index];
+
+    if (element == 1) {
+      frequencySet.push(totalFrequencySet[index]);
+    }
+  }
+
+  return frequencySet;
+}
+
+function sleep(ms) {
+  const wakeUpTime = Date.now() + ms;
+  while (Date.now() < wakeUpTime) {}
+}
--- Server/public/javascripts/student-subject.js
+++ Server/public/javascripts/student-subject.js
@@ -10,17 +10,15 @@ function changeButtonOff(btn) {
   btn.style.backgroundColor = '#00FFFF';
 }
 
-function submitData() {
-  var studentName = document.getElementById('student_name').value;
-  var studentId = document.getElementById('student_id').value;
-  console.log(studentName, studentId);
-  // 여기서 음파 정보 수집 후 분석해서 위 formData에 담긴 이름, 학번과 함께 서버로 fetch하기
+function submitData(subjectId) {
+  // 아래 함수에서 음파 정보 수집 후 분석해서 위 formData에 담긴 이름, 학번과 함께 서버로 fetch됨
+  startAttendanceCheck(subjectId);
 }
 
 function buttonClickHandler() {
   if (buttonFlag == false) {
     changeButtonOn(this);
-    submitData();
+    submitData(btn.id);
     // 여기서 서버 검증 결과에 따라 출석 실패/성공 띄우기
     buttonFlag = true;
   } else {
@@ -31,3 +29,179 @@ function buttonClickHandler() {
 
 var btn = document.getElementsByClassName('button_attendance')[0];
 btn.addEventListener('click', buttonClickHandler);
+
+// ---------------------------- 음파 검증 로직 ---------------------------- //
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+/**
+ * 출결 체크를 시작하는 function
+ *
+ * @param {number} subjectid  과목 코드
+ * @returns
+ */
+async function startAttendanceCheck(subjectid) {
+  await audioCtx.resume();
+
+  const name = document.getElementById('student_name').value;
+  const studentNumber = document.getElementById('student_id').value;
+
+  if (name == '') {
+    alert('이름을 입력해주세요.');
+    return;
+  }
+
+  if (studentNumber == '') {
+    alert('학번을 입력해주세요.');
+    return;
+  }
+
+  //MediaRecorder Setting
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+  });
+  const mediaRecorder = new MediaRecorder(mediaStream);
+
+  let audioArray = [];
+
+  mediaRecorder.ondataavailable = (e) => {
+    audioArray.push(e.data);
+  };
+
+  //Analyser Setting
+  const analyser = audioCtx.createAnalyser();
+
+  const audioSource = audioCtx.createMediaStreamSource(mediaStream);
+
+  audioSource.connect(analyser);
+
+  const dataArray = new Uint8Array(analyser.frequencyBinCount); //dataArray index = fftsize *frequency / samplerate
+
+  //onstop
+  mediaRecorder.onstop = () => {
+    const audioEl = document.querySelector('audio');
+    const blob = new Blob(audioArray, { type: 'audio/ogg codecs=opus' });
+    audioArray.splice(0);
+
+    const blobURL = window.URL.createObjectURL(blob);
+
+    audioEl.src = blobURL;
+
+    analyser.getByteFrequencyData(dataArray);
+  };
+
+  //Strat attendance check
+  let totalFrequencySet = generateFrequencySet();
+  const threshold = 20;
+  let attendanceCode = 0;
+
+  while (true) {
+    let count = 0;
+    while (count < 250) {
+      mediaRecorder.start();
+      sleep(10);
+      mediaRecorder.stop();
+
+      let inputAttendanceCode = frequencyToCode(
+        dataArray,
+        totalFrequencySet,
+        threshold,
+        analyser.fftSize
+      );
+
+      attendanceCode = inputAttendanceCode; //TODO: input 오류 처리 과정 추가
+
+      audioArray = [];
+      count++; // (검증 필요, 박지환이 추가)
+    }
+
+    // (Todo) 요기서 startTime 필드로 녹음 시작 시간 초단위 Unixtime으로 보내주시면 될 것 같습니다.
+    const requestForm = {
+      studentName: name,
+      studentId: studentNumber,
+      attendanceCode: attendanceCode,
+    };
+
+    const request = new Request('/student/' + subjectid.toString(), {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(requestForm),
+    });
+
+    const res = await fetch(request);
+
+    // 인증성공이면 서버에서 200 응답
+    if (res.status == 200) {
+      break;
+    }
+  }
+
+  alert('출석이 완료되었습니다.');
+}
+/**
+ *
+ * @param {number} codeLength
+ * @returns {Array} 출력 가능한 모든 주파수 set
+ */
+function generateFrequencySet(codeLength = 11) {
+  const minFrequency = 18000,
+    maxFrequency = 20000;
+
+  if (codeLength == 0) {
+    return;
+  }
+
+  let totalFrequencySet = Array(codeLength); // 해당 길이의 code가 출력할 수 있는 가능한 모든 주파수를 담은 Array
+
+  if (codeLength == 1) {
+    totalFrequencySet[0] = minFrequency;
+  } else {
+    let interval = Math.floor((maxFrequency - minFrequency) / (codeLength - 1)); //bit 길이에 맞도록 간격 설정
+
+    let cur = minFrequency;
+    for (let index = 0; index < totalFrequencySet.length; index++) {
+      //total_frequency_set 생성
+      totalFrequencySet[index] = cur;
+      cur += interval;
+    }
+  }
+
+  return totalFrequencySet;
+}
+
+/**
+ *
+ * @param {Array} inputFrequencySet
+ * @param {Array} totalFrequencySet
+ * @param {number} threshold
+ * @param {number} fftsize
+ * @returns authCode
+ */
+function frequencyToCode(
+  inputFrequencySet,
+  totalFrequencySet,
+  threshold,
+  fftsize
+) {
+  let attendanceCode = '';
+
+  for (let i = 0; i < totalFrequencySet.length; i++) {
+    const element = totalFrequencySet[i];
+
+    let index = Math.floor((fftsize * element) / 24000); //24000: default sample rate
+
+    if (inputFrequencySet[index] >= threshold) {
+      attendanceCode += '1';
+    } else {
+      attendanceCode += '0';
+    }
+  }
+
+  return attendanceCode;
+}
+
+function sleep(ms) {
+  const wakeUpTime = Date.now() + ms;
+  while (Date.now() < wakeUpTime) {}
+}
