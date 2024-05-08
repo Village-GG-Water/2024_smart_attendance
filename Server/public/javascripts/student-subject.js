9265858f93e781dc5518e4faf8a6160681532e4e
hw-ani <pcbmlh73@gmail.com>
2024-05-08 20:11:38 +0900

    Fix: 쓰레스홀드 수정

--- Server/public/javascripts/student-subject.js
+++ Server/public/javascripts/student-subject.js
@@ -265,7 +265,7 @@ function frequencyToCode(
 
     let index = Math.floor(((fftsize >> 1) * element) / 24000); //24000: default sample rate
 
-    if (inputFrequencySet[index] >= threshold) {
+    if (inputFrequencySet[index] >= 5) {
       attendanceCode += '1';
     } else {
       attendanceCode += '0';
