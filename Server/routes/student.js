abb35ba068871862251e766c896ebe171e462357
hw-ani <pcbmlh73@gmail.com>
2024-05-25 18:57:25 +0900

    Fix: 변수명 수정에 따른 로직 수정
    - 원래 저기 의미가 endTime 이었는데, startTime으로 돼있었음

--- Server/routes/student.js
+++ Server/routes/student.js
@@ -42,7 +42,7 @@ router.post('/:subjectId', function (req, res, next) {
   // 애초에 출석 코드 데이터가 없는 경우
   if (!result1[0].attendanceCode[attendanceCode]) return res.sendStatus(400);
   // 인증 기간이 만료된 출석 코드인 경우
-  if (Math.abs(result1[0].attendanceCode[attendanceCode] - startTime) > 5000)
+  if (Math.abs(result1[0].attendanceCode[attendanceCode] - endTime) > 5000)
     return res.sendStatus(400);
 
   // 인증에 성공한 경우 -> csv 저장 후, 응답
