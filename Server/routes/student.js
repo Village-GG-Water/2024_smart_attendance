bb478e5e21427a4d3ae596f2f30d0e99519315dd
hw-ani <pcbmlh73@gmail.com>
2024-05-30 18:52:21 +0900

    Fix: 디버깅

--- Server/routes/student.js
+++ Server/routes/student.js
@@ -47,10 +47,10 @@ router.post('/:subjectId', async function (req, res, next) {
 
   // #2. 출석코드 검증
   // 애초에 출석 코드 데이터가 없는 경우
-  if (!result1[0].attendanceCode[attendanceCode]) return res.sendStatus(400);
+  if (!result1[0].attendanceCode[attendanceCode]) return res.sendStatus(401);
   // 인증 기간이 만료된 출석 코드인 경우
   if (Math.abs(result1[0].attendanceCode[attendanceCode] - endTime) > 5000)
-    return res.sendStatus(400);
+    return res.sendStatus(402);
 
   // 인증에 성공한 경우 -> csv 저장 후, 응답
   result2[0].isAttended = true; // 출석으로 변경
